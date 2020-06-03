009b3e4ee34e78e5686216ed38fd8fcb42d69931
DiLiG1874 <dilig@foxmail.com>
2020-06-03 17:19:58 +0800

    re login

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,5 +1,10 @@
 import Vue from 'vue'
-import { Button, Form, FormItem, Input, Message } from 'element-ui'
+import { Button } from 'element-ui'
+import { Form } from 'element-ui'
+import { FormItem } from 'element-ui'
+import { Input } from 'element-ui'
+// 导入弹框提示
+import { Message } from 'element-ui'
 
 Vue.use(Button)
 Vue.use(Form)
