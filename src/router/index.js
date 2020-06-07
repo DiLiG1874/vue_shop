f41f4f14aafa4739c0eaf9723d835f9d510025b7
DiLiG1874 <dilig@foxmail.com>
2020-06-07 16:55:20 +0800

    welcome1

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '../components/Login.vue'
 import Home from '../components/Home.vue'
+import Welcome from '../components/Welcome.vue'
+import Users from '../components/user/Users.vue'
 
 Vue.use(VueRouter)
 
@@ -9,22 +11,28 @@ const router = new VueRouter({
   routes: [
     { path: '/', redirect: '/login' },
     { path: '/login', component: Login },
-    { path: '/home', component: Home },
+    {
+      path: '/home',
+      component: Home,
+      children: [
+        { path: '/welcome', component: Welcome },
+        { path: '/users', component: Users },
+      ],
+    },
   ],
 })
 
 // 挂载路由导航守卫
-router,
-  router.beforeEach((to, from, next) => {
-    // to 表示将要访问的路径
-    // from 表示从哪个路径跳转而来
-    // next 是一个函数，next()表示放行,next('/login')强制跳转
+router.beforeEach((to, from, next) => {
+  // to 表示将要访问的路径
+  // from 表示从哪个路径跳转而来
+  // next 是一个函数，next()表示放行,next('/login')强制跳转
 
-    if (to.path === '/login') return next()
-    // 获取 token
-    const tokenStr = window.sessionStorage.getItem('token')
-    if (!tokenStr) return next('/login')
-    next()
-  })
+  if (to.path === '/login') return next()
+  // 获取 token
+  const tokenStr = window.sessionStorage.getItem('token')
+  if (!tokenStr) return next('/login')
+  next()
+})
 
 export default router
